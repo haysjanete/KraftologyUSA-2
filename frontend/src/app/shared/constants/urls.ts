9a14a849b21bc23551f118b08d605bc41f3a31a0
haysjanete <74339100+haysjanete@users.noreply.github.com>
2023-08-07 20:18:50 -0500

    Login model added

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -5,3 +5,6 @@ export const CUPS_TAGS_URL = CUPS_URL + '/tags';
 export const CUPS_BY_SEARCH_URL = CUPS_URL + '/search/';
 export const CUPS_BY_TAG_URL = CUPS_URL + '/tag/';
 export const CUPS_BY_ID_URL = CUPS_URL + '/';
+
+
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
